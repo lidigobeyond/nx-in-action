ec38967b7eae6797c196b042650562ed0b7a98db
lidigobeyond <khkh4631@gmail.com>
2026-02-10 23:02:11 +0900

    feat: lib3 코드 수정

--- libs/lib3/src/lib/lib3.service.ts
+++ libs/lib3/src/lib/lib3.service.ts
@@ -1,4 +1,8 @@
 import {Injectable} from "@nestjs/common";
 
 @Injectable()
-export class Lib3Service {}
+export class Lib3Service {
+  random(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+}
